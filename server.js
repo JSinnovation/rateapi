e7c92d44e68f33910d31eac1dbb6b3ae95669fd5
JSinnovation <tekipaq@gmail.com>
2018-08-13 11:27:42 -0500

    port to 5000

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.use('/api', user);
 app.use('/api', company);//use middleware
 app.use('/api', file); 
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Server running on port 3000'); 
+app.listen(process.env.PORT || 5000, () => {
+    console.log('Server running on port 5000'); 
 });
 
