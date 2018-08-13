c6fcf08e19e335cc33eb50003f7b4180a97068c6
JSinnovation <tekipaq@gmail.com>
2018-08-13 14:09:31 -0500

    port troubleshoot

--- server.js
+++ server.js
@@ -57,12 +57,9 @@ app.use('/api', file);
     console.log('Server running on port 3000');
 }); */
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
-/* 
+
+
 app.listen(process.env.PORT || 3000, function(){
     console.log("Server running on port "+ process.env.PORT);
-}); */
+}); 
 
