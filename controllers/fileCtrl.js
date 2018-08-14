e5010fe912e19c068b95fa8422bf9e59d9b178c1
JSinnovation <tekipaq@gmail.com>
2018-08-14 09:59:36 -0500

    new technique cloudinary

--- controllers/fileCtrl.js
+++ controllers/fileCtrl.js
@@ -2,12 +2,20 @@ const User = require('../models/user');
 const Company = require('../models/company')
 const cloudinary = require('cloudinary-core');
 
+// Using the config function
+var cl = cloudinary.Cloudinary.new();
+cl.config( 
+  "cloud_name", process.env.CLOUD_NAME,
+  "api_key", process.env.API_KEY, 
+  "api_secret", process.env.API_SECRET 
+);
+/* 
 cloudinary.config({ 
     cloud_name: process.env.CLOUD_NAME, 
     api_key: process.env.API_KEY, 
     api_secret: process.env.API_SECRET 
   });
-
+ */
 exports.addImage = async(req, res) => {
 cloudinary.uploader.upload(req.body.image, (result) => {
 const savedData = async () => {
